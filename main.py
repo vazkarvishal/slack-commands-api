4546f06860191c18e2c87af7d1bd3f3d5be4f1d0
Vishal Vazkar <vazkarvishal@gmail.com>
2020-09-02 23:50:09 +0100

    handling form response form slack and converting to json response.

--- main.py
+++ main.py
@@ -18,8 +18,15 @@ def root_page():
 
 @app.post("/slack")
 async def initial_slack_payload(request: Request):
+
     form_data = await request.form()
-    print(form_data)
+    response_list = form_data.multi_items()
+    response_payload = {}
+    for item in response_list:
+        key, value = item
+        response_payload.update({key: value})
+
+    print(json.dumps(response_payload, indent= 4, sort_keys=True))
     response_message = {
       "response_type": "ephemeral",
       "text": ":v: We have received your request and working on it. :v:"
